709c6050d3c7cc26d6814a4cadb05a7753be12df
Scealang <54307873+Scealang@users.noreply.github.com>
2019-12-20 11:35:48 +0300

    Лабораторная работа 3

---
+++ Лабораторная работа 3/project.clj
@@ -0,0 +1,7 @@
+(defproject metrics-server "1.0.0"
+  :description "Sample server for stunents labwork"
+  :license {:name "Apache 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [clj-http "3.6.0"]
+                 [cheshire "5.5.0"]])
